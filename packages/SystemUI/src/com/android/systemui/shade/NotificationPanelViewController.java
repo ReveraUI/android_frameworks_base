f8ec27f717a17042338f260d8a12b7f0ecd082a2
Michael Bestas <mkbestas@lineageos.org>
2023-09-21 18:38:25 +0300

    Merge tag 'android-13.0.0_r75' into staging/lineage-20.0_merge-android-13.0.0_r75
    Android 13.0.0 release 75
    
    # -----BEGIN PGP SIGNATURE-----
    #
    # iF0EABECAB0WIQRDQNE1cO+UXoOBCWTorT+BmrEOeAUCZQiSvgAKCRDorT+BmrEO
    # eCrHAJ9W/MTfOMGyaxEfujTBFqkwW7FouQCbB5MeQpNgGalBC0RXhE7zHJdATmw=
    # =jKbp
    # -----END PGP SIGNATURE-----
    # gpg: Signature made Mon Sep 18 21:11:10 2023 EEST
    # gpg:                using DSA key 4340D13570EF945E83810964E8AD3F819AB10E78
    # gpg: Good signature from "The Android Open Source Project <initial-contribution@android.com>" [marginal]
    # gpg: initial-contribution@android.com: Verified 1890 signatures in the past
    #      22 months.  Encrypted 4 messages in the past 20 months.
    # gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    # gpg:          It is not certain that the signature belongs to the owner.
    # Primary key fingerprint: 4340 D135 70EF 945E 8381  0964 E8AD 3F81 9AB1 0E78
    
    # By Robin Lee (2) and others
    # Via Android Build Coastguard Worker
    * tag 'android-13.0.0_r75':
      Improve user handling when querying for resumable media
      Update AccountManagerService checkKeyIntentParceledCorrectly.
      Forbid granting access to NLSes with too-long component names
      Ignore virtual presentation windows - RESTRICT AUTOMERGE
      [DO NOT MERGE] Update quickshare intent rather than recreating
      Keyguard: use transition state for syncing occlude [RESTRICT AUTOMERGE]
      Cancel current animation instead of candidate
    
     Conflicts:
    	packages/SystemUI/src/com/android/systemui/screenshot/SaveImageInBackgroundTask.java
    	packages/SystemUI/tests/src/com/android/systemui/media/controls/resume/MediaResumeListenerTest.kt
    	packages/SystemUI/tests/src/com/android/systemui/media/controls/resume/ResumeMediaBrowserTest.kt
    	packages/SystemUI/tests/src/com/android/systemui/screenshot/SaveImageInBackgroundTaskTest.kt
    
    Change-Id: I9eaa285bb1e63d480358fefa55a6356cf138448a

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2875,7 +2875,6 @@ public final class NotificationPanelViewController implements Dumpable {
     /** Set whether the bouncer is showing. */
     public void setBouncerShowing(boolean bouncerShowing) {
         mBouncerShowing = bouncerShowing;
-        mNotificationStackScrollLayoutController.updateShowEmptyShadeView();
         updateVisibility();
     }
 
